79e63c7bdcb0281c396d0c4f807d0f06345fdac0
Samuel Sanchez <trueno.acero.metal@gmail.com>
2021-07-04 19:08:54 +0200

    Bynary Chop: Corrected GO version

--- BinaryChop/go/binarychop.go
+++ BinaryChop/go/binarychop.go
@@ -4,16 +4,16 @@ func chop(number int, array []int) int {
 	var left, right int
 	left = 0
 	right = len(array)
-	var found int = -1
+	var found int = -2
 	var middle int = (right + left) /2
-	for left != right || found != -1 {
+	for left != right || found != -2 {
 		if number < array[middle] {
 			left = middle +1 
 		}
 		else if number > array[middle] {
-			right = middle +1
+			right = middle - 1
 		}
-		else if number == array[middle]{
+		else if number == array[middle] || left == right {
 			found = array[middle]
 		}
 	}
